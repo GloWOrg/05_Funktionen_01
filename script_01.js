47bce8623009e4406c04998245177af9bbf5443a
GoloMeyer84 <golomeyer84@googlemail.com>
2022-01-19 09:26:39 +0100

    02 Refactoring hinzugefügt

--- script_01.js
+++ script_01.js
@@ -4,4 +4,43 @@
 
 // 1. Kapselung von Codeblöcken//
 
+//Funktionsaufruf (call) // kann auch vor der eigentlichen Kapselung stehen
+// test();
+
+//Funktionsrumpf (body) / callee
+function test()
+{
 console.log("Hallo Thilo");
+}
+// Funktionsaufrauf (call)
+test();
+
+//            FUNKTIONEN 02a                  //
+console.log("----------------------FUNKTIONEN 02a--------------------------------------");
+
+// ausgabeNamen();                 //call
+
+
+// function ausgabeNamen()
+//          //callee 
+// {
+// console.log("Hallo " + "Anna" + "!");   
+
+//Refactoring 
+//Außen (Ausgabe) bleibt gleich, nur der Code innen wird geändert
+
+// console.log("Hallo Anna!") bringt die gleiche Ausgabe wie console.log("Hallo " + "Anna" + "!")
+
+//Parametrisierung der Name wird durch eine Variable dargestellt
+ausgabeNamen();
+function ausgabeNamen()
+
+{
+let firstName = "Anna";
+console.log("Hallo " + firstName + "!");
+}
+//Die Variable firstName ist NUR innerhalb der Funktion gültig (scope)//
+
+
+
+
